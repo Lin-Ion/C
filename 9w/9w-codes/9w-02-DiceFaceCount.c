2650a549d119e3edf322a95ee6d8e76f21ec062b
Lin-Ion <20221085@edu.hanbat.ac.kr>
2022-10-25 22:49:02 +0900

    ANSI

---
+++ 9w/9w-codes/9w-02-DiceFaceCount.c
@@ -0,0 +1,28 @@
+﻿//02 DiceFace Count
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void get_dice_face() {
+	static int Face[6] = { 0 };
+	static count = 0;
+	
+	Face[rand() % 6] += 1;
+	count++;
+	if (count == 100)
+	{
+		for (int i = 0; i < 6; i++)
+			printf("%d->%d\n", i + 1, Face[i]);
+	}
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	for (int i = 0; i < 100; i++)
+		get_dice_face();
+	
+	return 0;
+}

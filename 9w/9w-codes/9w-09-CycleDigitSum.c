4c275ce14fce3cd4012112091dfde421fa0ed23e
Lin-Ion <20221085@edu.hanbat.ac.kr>
2022-10-28 11:56:56 +0900

    Add 9w-09, 10

---
+++ 9w/9w-codes/9w-09-CycleDigitSum.c
@@ -0,0 +1,24 @@
+﻿//자리수의 합
+
+#include <stdio.h>
+
+int get_digit_sum(int n);
+
+int main()
+{
+	int n;
+
+	printf("정수를 입력하시오: ");
+	scanf_s("%d", &n);
+
+	printf("자리수의 합: %d\n",get_digit_sum(n));
+
+	return 0;
+}
+
+int get_digit_sum(int n)
+{
+	if (n <= 9)
+		return n;
+	return n % 10 + get_digit_sum(n / 10);
+}

3657a9fbc76ee5e1ce1ed50dece70b45ae0bec42
Lin-Ion <20221085@edu.hanbat.ac.kr>
2022-10-28 13:39:18 +0900

    Fill 9w-10-Recursive

---
+++ 9w/9w-codes/9w-10-Recursive.c
@@ -0,0 +1,25 @@
+﻿// 1/1 + ... + 1/(n-1) + 1/n
+
+#include <stdio.h>
+
+double Recursive(int n);
+
+int main()
+{
+	int n;
+	
+	printf("정수를 입력하시오: ");
+	scanf_s("%d", &n);
+
+	printf("결과: %lf", Recursive(n));
+
+	return 0;
+}
+
+double Recursive(int n)
+{
+	if (n == 1)
+		return 1;	// + 1/1
+
+	return 1.0 / n + Recursive(n - 1); // 1/n + 1/(n-1)
+}

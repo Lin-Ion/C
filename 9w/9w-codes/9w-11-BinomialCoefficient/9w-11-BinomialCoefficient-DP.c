dc25d736e91613db813bd1006833c1c3b45d9896
Lin-Ion <20221085@edu.hanbat.ac.kr>
2022-10-30 02:02:56 +0900

    Add (DP)9w-11-BinomialCoefficient(.c and .py)

---
+++ 9w/9w-codes/9w-11-BinomialCoefficient/9w-11-BinomialCoefficient-DP.c
@@ -0,0 +1,27 @@
+﻿//이항 계수 ver.Dynamic Programming
+
+#include <stdio.h>
+
+int recursive_(int n, int k);
+
+int main_()
+{
+	int n, k;
+
+	printf("n = ");
+	scanf_s("%d", &n);
+	printf("k = ");
+	scanf_s("%d", &k);
+
+	printf("결과: %d\n", recursive(n, k));
+
+	return 0;
+}
+
+int recursive_(int n, int k)
+{
+	if (k == 0 | k == n)
+		return 1;
+
+	return recursive(n - 1, k - 1) + recursive(n - 1, k);
+}

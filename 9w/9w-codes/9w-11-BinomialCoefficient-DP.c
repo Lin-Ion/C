71b1e5fe7f8039a52e0684103f2086b8eb4004f7
Lin-Ion <20221085@edu.hanbat.ac.kr>
2022-10-30 03:58:16 +0900

    Add memoization in 9w-11-BinomialCoefficient-DP.c 재귀함수 압축, 동적배열 추가예정

---
+++ 9w/9w-codes/9w-11-BinomialCoefficient-DP.c
@@ -0,0 +1,29 @@
+﻿//이항 계수 ver.Dynamic Programming
+
+#include <stdio.h>
+
+int recursive(int n, int k);
+
+int main()
+{
+	int n, k;
+
+	printf("n = ");
+	scanf_s("%d", &n);
+	printf("k = ");
+	scanf_s("%d", &k);
+
+	int m[n][k];
+
+	printf("결과: %d\n", recursive(n, k));
+
+	return 0;
+}
+
+int recursive(int n, int k)
+{
+	if (k == 0 | k == n)
+		return 1;
+
+	return recursive(n - 1, k - 1) + recursive(n - 1, k);
+}

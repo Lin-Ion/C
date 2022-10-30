ac46f5f89321dc7856cf110127cc5ff364e6eec2
Lin-Ion <20221085@edu.hanbat.ac.kr>
2022-10-31 03:06:07 +0900

    Add 9w-12-Fibonacci.c and DP

---
+++ 9w/9w-codes/9w-12-Fibonacci-DP.c
@@ -0,0 +1,26 @@
+﻿//피보나치 DP
+
+#include <stdio.h>
+int fib(int n);
+
+int m[32] = { 0 };
+
+int main()
+{
+
+	for (int i = 0; i < 32; i++)
+		printf("fib(%d) = %d\n", i, fib(i));
+
+	return 0;
+}
+
+int fib(int n)
+{
+	if (n <= 1)
+		return n;
+
+	if (m[n])
+		return m[n];
+	else 
+		return m[n] = fib(n - 2) + fib(n - 1);
+}
---
+++ 9w/9w-codes/9w-12-Fibonacci.c
@@ -0,0 +1,21 @@
+﻿//피보나치
+
+#include <stdio.h>
+
+int m[64] = { 0 };
+
+int fib(int n);
+int main()
+{
+	for (int i = 0; i < 10; i++)
+		printf("fib(%d) = %d\n", i, fib(i));
+
+	return 0;
+}
+int fib(int n)
+{
+	if (n <= 1)
+		return n;
+	else
+		return fib(n - 2) + fib(n - 1);
+}

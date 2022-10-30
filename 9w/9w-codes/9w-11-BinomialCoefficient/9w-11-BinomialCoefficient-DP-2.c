b912cad850456f0ecdb25bbd2e9459655b2f73bc
Lin-Ion <20221085@edu.hanbat.ac.kr>
2022-10-30 14:54:56 +0900

    Add 9w-11-Bin..Coe..-DP-2.c

---
+++ 9w/9w-codes/9w-11-BinomialCoefficient/9w-11-BinomialCoefficient-DP-2.c
@@ -0,0 +1,32 @@
+﻿//이항 계수 ver.Dynamic Programming
+
+#include <stdio.h>
+#include <stdlib.h>
+int rec(int n, int k);
+
+int main()
+{
+	int n, k;
+
+	printf("n = ");
+	scanf_s("%d", &n);
+	printf("k = ");
+	scanf_s("%d", &k);
+
+	printf("결과: %d\n", rec(n, k));
+
+	return 0;
+}
+
+int rec(int n, int k)
+{
+	static m[65][65] = { 0 };
+
+	if (m[n][k])
+		return m[n][k];
+	if (k == 0 || k == n)
+		return m[n][k] = 1;
+
+
+	return m[n][k] = rec(n - 1, k - 1) + rec(n - 1, k);
+}

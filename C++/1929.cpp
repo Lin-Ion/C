b29aa2ff5951bd8fcd9ebfc7fa8641e91f2a8921
Lin-Ion <20221085@edu.hanbat.ac.kr>
2023-01-06 21:11:25 +0900

    Fix

--- C++/1929.cpp
+++ C++/1929.cpp
@@ -6,53 +6,56 @@ using namespace std;
 int main()
 {
 	//입력
-	int M = 2;
-	int N = 1000000;
+	int M = 0;
+	int N = 0;
 
 	cin >> M >> N;
 
 	//배열 생성
-	bool* A = new bool[N + 1](); //false로 초기화
+	bool* A = new bool[N + 1]();// false로 초기화
+
 
 	//소수찾기 시작
+	bool inRange = true;			
+	int max_i = ceil(sqrt(N));
+	int i = 2;
+	int j = 2;
+	int n_p = 0;
+
 	// i가 2인 경우
-	bool inRange = true;
-	int max_i = sqrt(N);
-	{
-		int i = 2;
-		int j = ceil((double)M / 2);
-		if (j < 2) {
-			j = 2;
-			inRange = false;
-		}
-
-		// int n_p == non_prime
-		for (int n_p = i * j; n_p <= N; n_p += i)
-			A[n_p] = true;
-	}
-
-	// i가 3이상인 경우
-	int j = ceil((double)M / 2);
-
-	if (j < 3) {
-		j = 3;
+	j = ceil((double)M / i);	// 시작이 M일 때, i*j는 M 이상이어야 함!
+	if (j < i) {
+		j = i;
 		inRange = false;
 	}
 
-	for (int i = 3; i < max_i; i += 2) {
+	// int n_p == non_prime
+	for (n_p = i * j; n_p <= N; n_p += i) {
+		A[n_p] = true;
+	}
 
-		for (int n_p = i * j; n_p <= N; n_p += i) {
-			cout << i << ", " << j << ", " << n_p << ", " << i * j << '\n';
+	// i가 3이상인 경우
+	for (i = 3; i < max_i; i += 2) {
+		if (inRange) {
+			j = ceil((double)M / i);
+			if (j < i) {
+				j = i;
+				inRange = false;
+			}
+		}
+		else j = i;
+
+		for (n_p = i * j; n_p <= N; n_p += i) {
 			A[n_p] = true;
 		}
 	}
 
-	//출력
-	for (int i = M; i <= N; ++i) {
+	// 출력
+	for (i = M; i <= N; ++i) {
 		if (A[i] == false)
 			cout << i << '\n';
 	}
 
-	//배열 갯수 조절 필요성 있음
+	// 배열 갯수 조절 필요성 있음
 	return 0;
 }
